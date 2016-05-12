2cd3390d6700ae2a2f3a3a4c80f043d4075e033e
Justincadburywong <justincadburywong@gmail.com>
2016-05-12 11:37:47 -0700

    update attr_accessor to include remaining_guessess

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,11 +1,12 @@
 class GuessingGame
 
-attr_accessor :congrats_message
+attr_accessor :congrats_message, :remaining_guesses
 
 	def initialize(secret_number, num_guesses)
 		@secret_number = secret_number
 		@num_guesses = num_guesses
 		@congrats_message = congrats_message
+		@remaining_guesses = remaining_guesses
 	end
 
 
