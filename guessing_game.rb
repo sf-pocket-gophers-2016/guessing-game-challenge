a2e7aee6505347b54815320d8690feca1d9f8e4f
Justin Wong & Abid Ramay <pair+justincadburywong.kasper341@devbootcamp.com>
2016-05-12 11:51:57 -0700

    update has_won and has_lost methods to return false upon startup, but allows for writability using attr_accessor

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,6 +1,7 @@
 class GuessingGame
 
 attr_accessor :congrats_message, :remaining_guesses
+attr_accessor :has_won, :has_lost
 
 	def initialize(secret_number, num_guesses)
 		@secret_number = secret_number
@@ -8,6 +9,14 @@ attr_accessor :congrats_message, :remaining_guesses
 		@congrats_message = congrats_message
 		@remaining_guesses = remaining_guesses
 	end
+
+  def has_won?
+    @won = false
+  end
+
+  def has_lost?
+    @lost = false
+  end
 
 
 end
